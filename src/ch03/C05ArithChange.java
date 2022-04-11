b938c4311328041acde5242d02ba94c373b246af
shinbo <sbg03202@naver.com>
2022-04-11 15:28:18 +0900

    2022-04-11

---
+++ src/ch03/C05ArithChange.java
@@ -0,0 +1,17 @@
+package ch03;
+
+public class C05ArithChange {
+	public static void main(String[] args) {
+		
+		int num = 10, div = 4;
+		
+		double result = num / div;
+		double result2 = (double)num /div;
+		
+		
+		
+		System.out.println("result : " + result);
+		System.out.println("result2 : " + result2);
+	}
+
+}
---
+++ src/ch03/C06StringTypeChange.java
@@ -0,0 +1,8 @@
+package ch03;
+
+public class C06StringTypeChange {
+	public static void main(String[] args) {
+		
+	}
+
+}
